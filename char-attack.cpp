aa79073e9468ac8f0d2f05144b2059b8bac26ffc
yes551 <141025070+yes551@users.noreply.github.com>
2024-03-24 00:21:47 +0330

    Create char-attack.cpp

---
+++ char-attack.cpp
@@ -0,0 +1,55 @@
+ void attack(nonfreindly* enemy){
+		int DAM=selah.use();
+        int Aim1=Aim;
+        Aim1*=moddifier("Aim");
+    	int aim_chance = generateRandomNumber(Aim1);
+        int bodypart;
+        if(stamina != 0){
+	        if(selah.durability>0){
+	        	if (aim_chance==0) { //miss
+	        	cout<<"missed"<<endl;
+	           	bodypart = 0;
+	           	return ;
+				}
+		        else if (aim_chance < 4){ //feet
+		           	bodypart = 1;
+		           	//effect
+				}
+		        else if (aim_chance < 10){ //body
+		            bodypart = 2;
+		            //effect
+				}
+				else if (aim_chance == 10){ //head
+					bodypart = 3;
+					//effect
+				}
+		        
+				if(selah.LongRange){
+                    
+					cout<<"ooooooooooooooooooooo"<<endl;
+		            DAM=DAM+DAM*(bodypart*0.2);
+		         
+		            enemy->gethit(DAM,bodypart);}
+	        	}
+		        
+		        else{
+                    //nesfe
+                    DAM*=moddifier("streght");
+		        	//effect
+		            DAM=DAM+(Strength*0.1)*DAM+(bodypart*0.05)*DAM;
+		            if(rand()%4){
+                        //counter zombie
+                    }
+                   
+		            enemy->gethit(DAM,bodypart);}
+	        	}
+		  
+	        }
+	        else{
+	        	DAM=10;
+	         
+		            enemy->gethit(DAM,bodypart);}
+	        	}
+	        		stamina = stamina - DAM*(0.5);
+			}}
+		
