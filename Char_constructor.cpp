b6bb2e2ed3a8d1ee23b451ea4a37334341bfdaa7
yes551 <141025070+yes551@users.noreply.github.com>
2024-03-24 00:49:03 +0330

    Create Char_constructor.cpp

---
+++ Char_constructor.cpp
@@ -0,0 +1,14 @@
+ Character(string namee, string characterClass) {
+        name = namee;
+        if (characterClass == "tank") {
+            Health = 7;
+            vigor = 5;
+            Strength = 7;
+            Aim = 1;
+            Knowledge = 0;
+            level=20;
+            HP=7*20;
+            stamina=5*20;
+        }
+    };
+
