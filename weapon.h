da847af6688a960d8ee02b1fa3780b671e366e6b
Merixrd <154606756+Merixrd@users.noreply.github.com>
2024-03-27 11:52:57 +0330

    Update weapon.h

--- weapon.h
+++ weapon.h
@@ -13,8 +13,39 @@ public:
     int DurabilityUsage;
 	int durability;
     bool LongRange;
+  Weapon(const Weapon& other) {
+        this->id = other.id;
+        this->type = other.type;
+        this->detail = other.detail;
+        this->DurabilityUsage = other.DurabilityUsage;
+        this->damage = other.damage;
+        this->durability = other.durability;
+        this->LongRange = other.LongRange;
+    }
     Weapon(){}
     Weapon(string ID){
+	      id = ID;
+     if (ID == "AK-47") {
+        LongRange = true;
+        damage = 20;
+        DurabilityUsage = 4;
+        type = "weapon"; 
+        durability = rand() % 32; 
+        detail = "An iconic assault rifle known for its reliability and durability. It packs a punch with its high damage output.";
+    } else if (ID == "shocker") {
+        LongRange = false;
+        damage = 5;
+        DurabilityUsage = 1;
+        type = "weapon"; 
+        durability = 50; 
+        detail = "A handheld electric shock device, great for stunning enemies but not effective for lethal damage.";
+    } else if (ID == "bow") {
+        LongRange = true;
+        damage = 70;
+        DurabilityUsage = 1;
+        type = "weapon"; 
+        durability = rand() % 8; 
+        detail = "A traditional bow and arrow, surprisingly effective even in modern times. Silent and deadly.";
         if(ID=="gun"){
         type="weapon";
         id=ID;
@@ -52,4 +83,5 @@ public:
         return damage * 20; // example
     }
 };
+
 #endif 
