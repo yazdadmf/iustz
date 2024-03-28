940946e78588df15e574ce299487450e344016e7
mehrab-amirloo <154887581+mehrab-amirloo@users.noreply.github.com>
2024-03-28 14:45:07 +0330

    Create item(old).cpp

---
+++ item(old).cpp
@@ -0,0 +1,72 @@
+Weapon::Weapon(string id){
+if(i="AK-47"){
+	
+	id=i;
+	l=false;
+	d=60;
+	du=1;
+	LongRange=l;
+	damage=d;
+	DurabilityUsage=du;
+	type="heavy"; 
+    durablity=200; 
+    detail="long durability is a good reason to buy it";
+    
+	}
+	
+	if(i="shocker"){
+	
+	id=i;
+	l=false;
+	d=5;
+	du=1;
+	LongRange=l;
+	damage=d;
+	DurabilityUsage=du;
+	type="2"; 
+    durablity=50; 
+    detail="not good enough for killing but using it would be fun";
+    
+	}
+	if(i="water gun"){
+	
+	id=i;
+	l=false;
+	d=1;
+	du=1;
+	LongRange=l;
+	damage=d;
+	DurabilityUsage=du;
+	type="2"; 
+    durablity=100; 
+    detail="it turns off the fire and remember that some animals hate water";
+    
+	}
+	if(i="bow"){
+	
+	id=i;
+	l=true;
+	d=70;
+	du=1;
+	LongRange=l;
+	damage=d;
+	DurabilityUsage=du;
+	type="1"; 
+    durablity=200; 
+    detail="i found it in a trash can it looks useable";
+    
+	}
+	if(i="Katana"){
+	
+	id=i;
+	l=false;
+	d=90;
+	du=1;
+	LongRange=l;
+	damage=d;
+	DurabilityUsage=du;
+	type="melee weapon"; 
+    durablity=90; 
+    detail="slice them like a piece of cake";
+    
+	}
