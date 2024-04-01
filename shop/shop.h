2ba990b7a69e787871e84d1b4740b0bc17144eec
yes551 <141025070+yes551@users.noreply.github.com>
2024-04-01 19:51:16 +0330

    Update shop.h

--- shop/shop.h
+++ shop/shop.h
@@ -7,7 +7,49 @@ class Party;
 class Shop{
 
 vector<Item*> inventory;
-void generateShopInventory(int numWeapons, int numMeds, int numArmor, int numThrowables);
+void generateShopInventory(int numWeapons, int numMeds, int numArmor, int numThrowables) {
+    vector<string> weaponIDs =  {"AK-47", "shocker", "bow", "Katana", "pistol", "Frying pan", "Rocket launcher",
+    "Mike Tyson Gloves", "Witcher steel sword", "Sniper rifle", "Machine gun", "stapler",
+    "Lucille", "Cane", "Axe", "Shotgun", "Electric Guitar", "Dagger", "Revolver",
+     "MTs-108", "Wrench", "Whip"};
+    vector<string> medsIDs = { "stamina1", "health1", "weedweed", "strenght1", "knowledge", "health1stamina1", 
+    "lean", "weed", "health1health1", "health2", "stamina1stamina1", "stamina2", 
+    "strenght2", "strength1strength1", "energydrink", "adrenalineshot"};
+    vector<string> armorIDs = {
+    "clownShoes", "clownJumpsuit", "clownHat", "samuraiHelmet", "samuraiChestplate", 
+    "samuraiLegGuards", "trashBin", "wheelchair", "flowerSkirt", "shrekTShirt", 
+    "bush", "militaryHelmet", "tacticalVest", "combatBoots", "scrapHelmet", 
+    "scrapChestplate", "scrapGreaves", "chainmailCoif", "plateArmor", "greavesSabatons"};
+    vector<string> throwablesIDs = { "molotov","grenade","kitty","batrang","slippers",
+        "snowball","rock","oxford dictionary","peel of banana","shuriken","smoke bomb"};
+
+    srand(time(NULL));
+
+    for (int i = 0; i < numWeapons; ++i) {
+        string randomWeaponID = weaponIDs[rand() % weaponIDs.size()];
+        inventory.push_back(new Weapon(randomWeaponID));
+  
+    }
+
+    for (int i = 0; i < numMeds; ++i) {
+        string randomMedsID = medsIDs[rand() % medsIDs.size()];
+        inventory.push_back(new Meds(randomMedsID));
+       
+    }
+
+    for (int i = 0; i < numArmor; ++i) {
+        string randomArmorID = armorIDs[rand() % armorIDs.size()];
+        inventory.push_back(new Armor(randomArmorID));
+       
+    }
+
+    for (int i = 0; i < numThrowables; ++i) {
+        string randomThrowablesID = throwablesIDs[rand() % throwablesIDs.size()];
+        inventory.push_back(new Throwables(randomThrowablesID));
+       
+    }
+}
+
  void buyItem(Party& party, int itemIndex) ;
     void sellItem(Party& party, int itemIndex) ;
 
